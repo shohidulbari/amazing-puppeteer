d0db2e97fd2b278e2b787c472a3d51267f640d3d
shohidulbari <shohidulbari18@gmail.com>
2022-05-11 14:43:33 +0600

    added template using ejs

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParsar = require('cookie-parser');
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParsar());
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
